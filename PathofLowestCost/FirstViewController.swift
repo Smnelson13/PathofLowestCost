7d2307927b945d6ab062241b131011cf9a7da332
Shane Nelson <swiftyshane@gmail.com>
2017-08-29 11:29:07 -0400

    Initial Commit

---
+++ PathofLowestCost/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PathofLowestCost
+//
+//  Created by Shane Nelson on 8/29/17.
+//  Copyright © 2017 Shane Nelson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
