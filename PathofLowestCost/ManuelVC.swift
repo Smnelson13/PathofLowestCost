fad43679b99fe71386ced19a788d37cd98358be8
Shane Nelson <swiftyshane@gmail.com>
2017-08-29 22:20:47 -0400

    initial commit

---
+++ PathofLowestCost/ManuelVC.swift
@@ -0,0 +1,74 @@
+//
+//  ManuelVC.swift
+//  PathofLowestCost
+//
+//  Created by Shane Nelson on 8/29/17.
+//  Copyright © 2017 Shane Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ManuelVC: UIViewController
+{
+  @IBOutlet weak var a1: UITextField!
+  @IBOutlet weak var a2: UITextField!
+  @IBOutlet weak var a3: UITextField!
+  @IBOutlet weak var a4: UITextField!
+  @IBOutlet weak var a5: UITextField!
+  @IBOutlet weak var a6: UITextField!
+  @IBOutlet weak var b1: UITextField!
+  @IBOutlet weak var b2: UITextField!
+  @IBOutlet weak var b3: UITextField!
+  @IBOutlet weak var b4: UITextField!
+  @IBOutlet weak var b5: UITextField!
+  @IBOutlet weak var b6: UITextField!
+  @IBOutlet weak var c1: UITextField!
+  @IBOutlet weak var c2: UITextField!
+  @IBOutlet weak var c3: UITextField!
+  @IBOutlet weak var c4: UITextField!
+  @IBOutlet weak var c5: UITextField!
+  @IBOutlet weak var c6: UITextField!
+  @IBOutlet weak var d1: UITextField!
+  @IBOutlet weak var d2: UITextField!
+  @IBOutlet weak var d3: UITextField!
+  @IBOutlet weak var d4: UITextField!
+  @IBOutlet weak var d5: UITextField!
+  @IBOutlet weak var d6: UITextField!
+  @IBOutlet weak var e1: UITextField!
+  @IBOutlet weak var e2: UITextField!
+  @IBOutlet weak var e3: UITextField!
+  @IBOutlet weak var e4: UITextField!
+  @IBOutlet weak var e5: UITextField!
+  @IBOutlet weak var e6: UITextField!
+  
+  @IBAction func autoFillButton(_ sender: Any)
+  {
+    
+  }
+  
+  @IBAction func clearTextfieldsButton(_ sender: Any)
+  {
+    
+  }
+  
+  @IBAction func calculateButton(_ sender: Any)
+  {
+    
+  }
+ 
+
+  override func viewDidLoad()
+  {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view.
+  }
+
+  override func didReceiveMemoryWarning()
+  {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+  }
+  
+
+}
